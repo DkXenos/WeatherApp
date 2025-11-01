526f26b5070616340496a0883b4d633d620fbec0
DkXenos <superjason101010@gmail.com>
2025-11-01 15:58:40 +0700

    init

---
+++ app/src/main/java/com/jason/week7lab/uiview/Nomer1View.kt
@@ -0,0 +1,18 @@
+package com.jason.week7lab.uiview
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Soal1View(){
+    Text("Hello World")
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun Soal1Preview(){
+    Soal1View()
+}
+
+
